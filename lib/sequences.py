54094fdadb7af9304875a06f0d1c27c41135fc3d
stephenmaina2995 <stephenmaina2995@gmail.com>
2023-05-16 09:40:55 +0300

    completed assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
+# def print_fibonacci(length):
+#     # pass
+#     print({length})
+#     print_fibonacci(10)
 def print_fibonacci(length):
-    pass
+    fib_sequence = [0, 1]  # Initialize the sequence with the first two Fibonacci numbers
+    while len(fib_sequence) < length:
+        next_number = fib_sequence[-1] + fib_sequence[-2]  # Calculate the next Fibonacci number
+        fib_sequence.append(next_number)  # Add the next number to the sequence
+    print(fib_sequence)
+
+# Example usage:
+print_fibonacci(10)
